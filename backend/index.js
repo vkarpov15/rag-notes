49a309ff3fd563083898a36783c7db4e36ac7034
Valeri Karpov <val@karpov.io>
2024-02-01 14:03:13 -0500

    throttle requests that use OpenAI to prevent API key abuse

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,33 @@ module.exports = async function backend() {
   const app = addAsync(express());
   app.use(express.json());
 
-  app.putAsync('/answerQuestion', answerQuestion);
+  app.putAsync('/answerQuestion', limitTo100Requests, answerQuestion);
   app.getAsync('/countNotes', countNotes);
-  app.putAsync('/saveNote', saveNote);
+  app.putAsync('/saveNote', limitTo100Requests, saveNote);
 
   return { app };
 };
+
+let last100Requests = [];
+function limitTo100Requests(req, res, next) {
+  if (last100Requests.length < 100) {
+    last100Requests.push({
+      date: new Date(),
+      url: req.url
+    });
+
+    next();
+  } else {
+    const oldestRequest = last100Requests[0];
+    if (Date.now() - oldestRequest.date.valueOf() <= 1000 * 60 * 60) {
+      res.status(400).send('Rate limit exceeded, API only supports 100 requests/hour currently');
+      return;
+    }
+    last100Requests.shift();
+    last100Requests.push({
+      date: new Date(),
+      url: req.url
+    });
+    next();
+  }
+}
